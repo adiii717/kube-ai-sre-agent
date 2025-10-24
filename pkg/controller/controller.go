42d0add6c7f5a12418c4539f74178480213f2796
adiii717 <adilm717@gmail.com>
2025-10-24 15:02:28 +0500

    fix(controller): remove unused import

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -11,7 +11,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
